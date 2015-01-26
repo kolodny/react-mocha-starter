9ac4055e214f053ccc60e16519324f91e5aee553
Moshe Kolodny <mkolodny@integralads.com>
2015-01-26 14:05:05 -0500

    trying to get testling working

--- test-setup.js
+++ test-setup.js
@@ -1,7 +1,32 @@
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP && oThis ?
+                  this: 
+                  oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 if (typeof document === 'undefined') {
   var jsdom = require("jsdom").jsdom;
   global.document = jsdom('<!doctype html><html><body></body></html>');
   global.window = document.parentWindow;
   global.navigator = {userAgent: ''};
-  // ... add whatever browser globals your tests might need ...		
+  // ... add whatever browser globals your tests might need ...   
 }
