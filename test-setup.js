f95c16396b88428a220023b67ec88d348b12d61c
Moshe Kolodny <mkolodny@integralads.com>
2015-01-26 17:23:13 -0500

    added tesling support

--- test-setup.js
+++ test-setup.js
@@ -1,32 +1,41 @@
+// for phantomjs, taken from https://github.com/facebook/react/blob/e6672a3a03b10528c5dd492b5c88183637910502/src/test/phantomjs-shims.js
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function(oThis) {
-    if (typeof this !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+  // PhantomJS doesn't support Function.prototype.bind natively, so
+  // polyfill it whenever this module is required.
+  Function.prototype.bind = function(context) {
+    var func = this;
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    function bound() {
+      var invokedAsConstructor = func.prototype && (this instanceof func);
+      return func.apply(
+        // Ignore the context parameter when invoking the bound function
+        // as a constructor. Note that this includes not only constructor
+        // invocations using the new keyword but also calls to base class
+        // constructors such as BaseClass.call(this, ...) or super(...).
+        !invokedAsConstructor && context || this,
+        args.concat(Array.prototype.slice.call(arguments))
+      );
     }
 
-    var aArgs   = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP    = function() {},
-        fBound  = function() {
-          return fToBind.apply(this instanceof fNOP && oThis ?
-                  this: 
-                  oThis,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
+    // The bound function must share the .prototype of the unbound
+    // function so that any object created by one constructor will count
+    // as an instance of both constructors.
+    bound.prototype = func.prototype;
 
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
+    return bound;
   };
+
 }
 
-if (typeof document === 'undefined') {
+if (typeof document === 'undefined' ) {
   var jsdom = require("jsdom").jsdom;
   global.document = jsdom('<!doctype html><html><body></body></html>');
   global.window = document.parentWindow;
   global.navigator = {userAgent: ''};
+
+  // we want to be able to include jsx files from node
+  require('node-jsx').install();
+
   // ... add whatever browser globals your tests might need ...   
 }
--- test/index.js
+++ test/index.js
@@ -1,8 +1,4 @@
-try {
-  require('node-jsx').install();
-} catch(e) {
-  require('../test-setup');
-}
+require('../test-setup');
 var assert = require('assert');
 var React = require('react');
 // var ReactAddons = require('react/addons');
@@ -13,27 +9,27 @@ var HelloWorld = require('../component');
 describe('The HelloWorld component', function() {
   var component;
 
-  // beforeEach(function(done) {
-  //   component = React.render(
-  //     React.createElement(HelloWorld, {}),
-  //     document.body, function() {
-  //       setTimeout(done);
-  //     }
-  //   );
-  // });
+  beforeEach(function(done) {
+    component = React.render(
+      React.createElement(HelloWorld, {}),
+      document.body, function() {
+        setTimeout(done);
+      }
+    );
+  });
 
-  // afterEach(function(done) {
-  //   React.unmountComponentAtNode(document.body);
-  //   setTimeout(done);
-  // });
+  afterEach(function(done) {
+    React.unmountComponentAtNode(document.body);
+    setTimeout(done);
+  });
 
   it('should render a HelloWorld component', function() {
-    // assert.equal(component.getDOMNode().textContent, 'Hello World!');
+    assert.equal(component.getDOMNode().textContent, 'Hello World!');
   });
 
   it('should change when a prop changes', function() {
-    // component.setProps({name: 'Moshe'});
-    // assert.equal(component.getDOMNode().textContent, 'Hello Moshe!');
+    component.setProps({name: 'Moshe'});
+    assert.equal(component.getDOMNode().textContent, 'Hello Moshe!');
   });
 
 });
